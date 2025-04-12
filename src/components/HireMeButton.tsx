3bd466e349c2a0957bc8e37708f4da7c1085efd8
Rohit Rathore <rohit.rathore@universityliving.com>
2025-04-12 15:05:20 +0530

    portfolio section added

--- src/components/HireMeButton.tsx
+++ src/components/HireMeButton.tsx
@@ -7,15 +7,24 @@ const { BUTTON } = HEADER;
 
 interface HireMeButtonProps {
   className?: string;
+  buttonName?: string;
+  link?: string;
+  target?: string;
 }
 
-export default function HireMeButton({ className = "" }: HireMeButtonProps) {
+export default function HireMeButton({
+  className = "",
+  buttonName = "",
+  link = "",
+  target = '_self'
+}: HireMeButtonProps) {
   return (
     <Link
       className={`text-lg text-red-600 hover:text-white hover:from-red-600 transition-all duration-300 hover:scale-105 hover:to-red-600 shadow-lg bg-gradient-to-br from-slate-300 to-slate-50 py-2 px-8 rounded-lg ${className}`}
-      href={BUTTON.id}
+      href={link || BUTTON.id}
+      target={target}
     >
-      {BUTTON.name}
+      {buttonName || BUTTON.name}
     </Link>
   );
 }
